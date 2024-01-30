2df436a69fade9ff850c22c59ab0630599293dea
reavywilldan <reavywilldan@gmail.com>
2024-01-30 21:14:46 +0700

    first commit

---
+++ components/Sidebar.vue
@@ -0,0 +1,108 @@
+<script setup>
+let isOpen = ref(false)
+
+function toggleMenu() {
+  isOpen = !isOpen
+}
+</script>
+
+<template>
+  <aside class="sidebar" :class="{ active: isOpen }">
+    <div class="sidebar-info">
+      <figure class="avatar-box">
+        <img src="/images/loading.png" alt="Photo">
+      </figure>
+
+      <div class="info-content">
+        <h1 class="name" title="Reavy Willdan Kurniawan">
+          Reavy Willdan
+        </h1>
+
+        <p class="title text-center xl:block flex items-center justify-center gap-1">
+          <span class="xl:after:content-[''] after:content-[',']">Back End Developer</span>
+          <span class="xl:after:content-[''] after:content-[',']">Full Stack Developer</span>
+          <span>Beat Maker</span>
+        </p>
+      </div>
+
+      <button class="info_more-btn" @click="isOpen = !isOpen">
+        <span>Show Contacts</span>
+
+        <ion-icon name="chevron-down" />
+      </button>
+    </div>
+
+    <div class="sidebar-info_more">
+      <div class="separator" />
+
+      <ul class="contacts-list">
+        <li class="contact-item">
+          <div class="icon-box">
+            <ion-icon name="logo-twitter" />
+          </div>
+
+          <div class="contact-info">
+            <p class="contact-title">
+              Twitter
+            </p>
+
+            <a href="https://twitter.com/agcrisbp" class="contact-link" target="_blank">agcrisbp</a>
+          </div>
+        </li>
+
+        <li class="contact-item">
+          <div class="icon-box">
+            <ion-icon name="logo-github" />
+          </div>
+
+          <div class="contact-info">
+            <p class="contact-title">
+              Github
+            </p>
+
+            <a href="https://github.com/agcrisbp" class="contact-link" target="_blank">agcrisbp</a>
+          </div>
+        </li>
+
+        <li class="contact-item">
+          <div class="icon-box">
+            <ion-icon name="logo-linkedin" />
+          </div>
+
+          <div class="contact-info">
+            <p class="contact-title">
+              Linkedin
+            </p>
+
+            <a href="https://id.linkedin.com/in/agcrisbp" class="contact-link" target="_blank">Reavy Willdan Kurniawan</a>
+          </div>
+        </li>
+
+        <li class="contact-item">
+          <div class="icon-box">
+            <ion-icon name="location-outline" />
+          </div>
+
+          <div class="contact-info">
+            <p class="contact-title">
+              Location
+            </p>
+
+            <address>Indonesia</address>
+          </div>
+        </li>
+      </ul>
+
+      <div class="separator" />
+
+      <ul class="social-list">
+        <li class="social-item">
+          <NuxtLink to="mailto:agcrisbp@email.com" class="social-link" target="_blank">
+            <ion-icon name="mail-outline" />
+          </NuxtLink>
+        </li>
+        <LangSwitcher2 />
+      </ul>
+    </div>
+  </aside>
+</template>
